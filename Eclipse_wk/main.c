49dced57917611ed1e3994a48029c0941ec7d728
OmarMahmoudElsherif <104269847+OmarMahmoudElsherif@users.noreply.github.com>
2022-11-08 00:50:13 +0200

    Add files via upload

---
+++ Eclipse_wk/main.c
@@ -0,0 +1,55 @@
+/*
+ * main.c
+ *
+ *  Created on: 11 Oct 2022
+ *      Author: Omar Elsherif
+ *      FaceBook name : Omar Elsherif
+ */
+
+#include"lcd.h"
+#include"ultrasonic_sensor.h"
+#include<avr/io.h>
+
+
+int main(void)
+{
+
+	uint16 distance;
+
+	/* Enable Global Interrupt I-Bit */
+	SREG |= (1<<7);
+
+	/* Initialize LCD driver */
+	LCD_init();
+
+	/* Initialize Ultrasonic sensor */
+	Ultrasonic_init();
+
+
+	LCD_displayString("Distance=    cm");
+
+
+	while(1)
+	{
+		/*	Read distance from ultrasonic sensor*/
+		distance = Ultrasonic_readDistance();
+		LCD_moveCursor(0, 10);
+
+		/*	Display Distance on LCD*/
+		if(distance>=100)
+		{
+			LCD_intgerToString(distance);
+		}
+		else
+		{
+			LCD_intgerToString(distance);
+			LCD_displayCharacter(' ');
+		}
+
+	}
+
+
+
+	return 0;
+}
+
